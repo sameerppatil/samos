335d6360be297acbf055e75089bf8c28afadb745
Sameer Patil <sameerpatil38@gmail.com>
2016-11-18 16:56:36 -0700

    Cosmetic changes

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -2,20 +2,20 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
- 
+
 #include <kernel/tty.h>
- 
+
 #include "vga.h"
- 
+
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
 static uint16_t* const VGA_MEMORY = (uint16_t*) 0xB8000;
- 
+
 static size_t terminal_row;
 static size_t terminal_column;
 static uint8_t terminal_color;
 static uint16_t* terminal_buffer;
- 
+
 void terminal_initialize(void) {
 	terminal_row = 0;
 	terminal_column = 0;
@@ -28,16 +28,16 @@ void terminal_initialize(void) {
 		}
 	}
 }
- 
+
 void terminal_setcolor(uint8_t color) {
 	terminal_color = color;
 }
- 
+
 void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
- 
+
 void terminal_putchar(char c) {
 	unsigned char uc = c;
 	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
@@ -47,12 +47,12 @@ void terminal_putchar(char c) {
 			terminal_row = 0;
 	}
 }
- 
+
 void terminal_write(const char* data, size_t size) {
 	for (size_t i = 0; i < size; i++)
 		terminal_putchar(data[i]);
 }
- 
+
 void terminal_writestring(const char* data) {
 	terminal_write(data, strlen(data));
 }
